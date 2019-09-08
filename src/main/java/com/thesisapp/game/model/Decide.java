8af159e7ff8107c5d788418f90d200ea964cbbc8
Kyle Hobbs <kyle.hobbs01@gmail.com>
2019-09-08 12:22:54 -0600

    Added a Decide class which handles when a player makes a decision by choosing the next node

---
+++ src/main/java/com/thesisapp/game/model/Decide.java
@@ -0,0 +1,27 @@
+package com.thesisapp.game.model;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+public class Decide implements Serializable {
+
+  private int[] decisionList;
+
+  public Decide() { this.decisionList = new int[0]; }
+
+  public Decide(int[] decisionList) { this.decisionList = decisionList; }
+
+  //Method which returns a SimpleNode object that is sent to the server
+  public SimpleNode getNode() {
+    //Use the Game class to get the root node for the entire story tree
+    Node rootNode = (new Game()).buildFullStoryTree();
+
+    //Traverse the story tree using the decisionList to get to the current node
+    Node currentNode = rootNode;
+    for (int whichChild : this.decisionList) {
+      currentNode = currentNode.getChild(whichChild);
+    }
+
+    return new SimpleNode(currentNode);
+  }
+}
