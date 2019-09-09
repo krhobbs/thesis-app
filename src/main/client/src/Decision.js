715ec5c1ab4b4565f72647e09a3c421fa2b11d32
Kyle Hobbs <kyle.hobbs01@gmail.com>
2019-09-08 22:34:41 -0600

    Added Decision and Message component skeletons

---
+++ src/main/client/src/Decision.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { ButtonGroup, Button } from 'reactstrap';
+
+class Decision extends Component {
+
+}
+
+export default Decision;
--- src/main/client/src/Home.js
+++ src/main/client/src/Home.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      previouslyPlayed: [],
+      currentNode: {
+
+      },
+      decisionList: []
+    };
 
   }
 }
