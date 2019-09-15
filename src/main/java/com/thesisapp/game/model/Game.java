d448864e4ef832fa4b3b273f7d64c6e39670c54a
Kyle Hobbs <kyle.hobbs01@gmail.com>
2019-09-14 20:28:10 -0600

    Added more story nodes for act i

--- src/main/java/com/thesisapp/game/model/Game.java
+++ src/main/java/com/thesisapp/game/model/Game.java
@@ -139,12 +139,193 @@ public class Game {
     Node n29 = new Node(29, "Look, you don't want the details, Simon. When I say \"long story,\" that implies brutal, uncomfortable, and dangerous to know.", n29d, 1, false);
 
     Decision n30d1 = new Decision("n30d1", "So, mystery guy. Can I ask what happened?", 0, 0);
-    Decision[] n30d = {n30d1};
+    Decision n30d2 = new Decision("n30d2", "Well, can you at least hint at what happened?", 0, 0);
+    Decision[] n30d = {n30d1, n30d2};
     Node n30 = new Node(30, "You do that.", n30d, 1, false);
 
+    Decision n31d1 = new Decision("n31d1", "I wanted to teach myself a little something about building tech. Starting way back when, with a cathode-ray tube computer.", 0, 0);
+    Decision n31d2 = new Decision("n31d2", "I get bored. I figured it would be neat to put on of these cathode-ray tube computers together.", 1, 0);
+    Decision[] n31d = {n31d1, n31d2};
+    Node n31 = new Node(31, "Why were you tinkering with scrap.", n31d, 1, false);
 
+    Decision n32d1 = new Decision("n32d1", "Is that a good thing?", 0, 0);
+    Decision n32d2 = new Decision("n32d2", "Does that mean we can be friends?", 0, 0);
+    Decision[] n32d = {n32d1, n32d2};
+    Node n32 = new Node(32, "Ambitious type, huh? You sound like a lot of people I know.", n32d, 1, false);
 
+    Node n33 = new Node(33, "Tinkering for its own sake, then. You sound like a lot of people I know.", n32d, 1, false);
 
+    Decision n34d1 = new Decision("n34d1", "I suppose I understand keeping things close to the vest. But where do we go from here?", 0, 0);
+    Decision n34d2 = new Decision("n34d2", "All this redacted info is really supporting the \"seedy corporation\" vibes, you know. Where do we go from here, then?", 0, 0);
+    Decision[] n34d = {n34d1, n34d2};
+    Node n34 = new Node(34, "TBD.", n34d, 1, false);
+
+    Decision n35d1 = new Decision("n35d1", "We're mysteriously connected via an ancient computer and a crazy brain chip. I'd like to know why, but it doesn't seem like that's possible. So I don't see what else we can do here.", 0, 0);
+    Decision n35d2 = new Decision("n35d2", "We're mysteriously connected via an ancient computer and a crazy brain chip. You're not being too helpful in connecting dots, so I don't see what else we can do here.", 0, 0);
+    Decision[] n35d = {n35d1, n35d2};
+    Node n35 = new Node(35, "What do you mean?", n35d, 1, false);
+
+    Decision n36d1 = new Decision("n35d1", "Not at all. This has been interesting, to say the least.", 0, 0);
+    Decision n36d2 = new Decision("n35d2", "TBD.", 1, 0);
+    Decision[] n36d = {n36d1, n36d2};
+    Node n36 = new Node(36, "Am I that bad a conversationalist?", n36d, 1, false);
+
+    Node n37 = new Node(37, "Ha, ha.", emptyDecisions, 1, false);
+
+    Decision n38d1 = new Decision("n38d1", "Trouble? What can I do to help?", 0, 0);
+    Decision n38d2 = new Decision("n38d2", "I sense a bad \"all tied up\" pun coming. Evil corporation or government agency? I knew it.", 1, 0);
+    Decision[] n38d = {n38d1, n38d2};
+    Node n38 = new Node(38, "Okay. You could say I'm in a bit of bind.", n38d, 1, false);
+
+    Node n39 = new Node(39, "Thank you, [Simon]. That you're even willing to listen... It means a lot.", emptyDecisions, 1, false);
+
+    Node n40 = new Node(40, "Maybe there is something you could do.", emptyDecisions, 1, false); //Checkpoint?
+
+    Node n41 = new Node(41, "No, [Simon], you don't know. And there's no reason you should.", emptyDecisions, 1, false);
+
+    Decision n42d1 = new Decision("n42d1", "Wait, I'm sorry. I don't want to sign off. So, the bind. Is it something I can help with?", 0, 0);
+    Decision n42d2 = new Decision("n42d2", "I don't want to sign off. I want to know more. So, the bind. Is it something I can help with?", 0, 0);
+    Decision[] n42d = {n42d1, n42d2};
+    Node n42 = new Node(42, "Look, if you want to sign off, then sign off. I'm used to the silence.", n42d, 1, false);
+
+    Node n43 = new Node(43, "There's a message I need to deliver. It would be best if you didn't leave an electronic trail, so if you could write a letter to someone, I could rest easy.", emptyDecisions, 1, false);
+
+    Decision n44d1 = new Decision("n44d1", "I can do that! Why can't you though?", 0, 0);
+    Decision n44d2 = new Decision("n44d2", "Snail mail, huh? If I remember how to send a letter, I will. Any reason you can't, though?", 0, 0);
+    Decision[] n44d = {n44d1, n44d2};
+    Node n44 = new Node(44, "So to speak.", n44d, 1, false);
+
+    Node n45 = new Node(45, "I would if I could.", emptyDecisions, 1, false);
+
+    Decision n46d1 = new Decision("n46d1", "That's tough. I'll do what I can, but it won't really remain private this way, either.", 0, 0);
+    Decision n46d2 = new Decision("n46d2", "Well, if you want me to pen a letter for you old school, it's not going to be private anyway.", 0, 0);
+    Decision[] n46d = {n46d1, n46d2};
+    Node n46 = new Node(46, "Nothing I would manage to send out would remain private.", n46d, 1, false);
+
+    Node n47 = new Node(47, "That is true...", emptyDecisions, 1, false);
+
+    Decision n48d1 = new Decision("n48d1", "What could happen?", 0, 0);
+    Decision n48d2 = new Decision("n48d2", "Let me worry about my own sake.", 1, 0);
+    Decision[] n48d = {n48d1, n48d2};
+    Node n48 = new Node(48, "I don't know if this is wise after all. For your own sake.", n48d, 1, false);
+
+    Node n49 = new Node(49, "I'm not sure.", emptyDecisions, 1, false);
+    Node n50 = new Node(50, "It could be dangerous for you because...", emptyDecisions, 1, false);
+    Node n51 = new Node(51, "Well, I suppose there's no hiding it anymore.", emptyDecisions, 1, false);
+
+    Node n52 = new Node(52, "It's not as it seems, and there's a lot going on here I don't think you could understand.", emptyDecisions, 1, false); //Merge point
+
+    Node n53 = new Node(53, "All right. If you're sure.", emptyDecisions, 1, false);
+    Node n54 = new Node(54, "Then I suppose you need to know...", emptyDecisions, 1, false);
+
+    Decision n55d1 = new Decision("n55d1", "Oh. Well. That's odd.", 0, 0);
+    Decision n55d2 = new Decision("n55d2", "...", 1, 0);
+    Decision[] n55d = {n55d1, n55d2};
+    Node n55 = new Node(55, "But you should know that I'm in a prison -- of sorts.", n55d, 1, false);
+
+    Decision n56d1 = new Decision("n56d1", "Did you do it? Whatever you're in for?", 0, 0);
+    Decision n56d2 = new Decision("n56d2", "You have to realize I'm going to ask what you did to get put in prison.", 0, 0);
+    Decision[] n56d = {n56d1, n56d2};
+    Node n56 = new Node(56, "By all means, take your time.", n56d, 1, false);
+
+    Decision n57d1 = new Decision("n57d1", "I'm thinking.", 0, 0);
+    Decision n57d2 = new Decision("n57d2", "......", 0, 0);
+    Decision[] n57d = {n57d1, n57d2};
+    Node n57 = new Node(57, "Did you just type ellipses?", n57d, 1, false);
+
+    Node n58 = new Node(58, "That's complicated.", emptyDecisions, 1, false);
+
+    Decision n59d1 = new Decision("n59d1", "Okay. I guess you don't seem to have a reason to lie. I'll write this letter. But I won't promise to send it, if I think it would upset someone.", 0, 0);
+    Decision n59d2 = new Decision("n59d2", "That's...pretty vague. I'll write this letter, Prosper. But I'm not promising to send it until I know more.", 0, 0);
+    Decision[] n59d = {n59d1, n59d2};
+    Node n59 = new Node(59, "I did what I thought was right. I didn't hurt anyone, and I definitely don't want you to get hurt, [Simon].", n59d, 1, false);
+
+    Node n60 = new Node(60, "I suppose I can't ask for more. Thanks, [Simon].", emptyDecisions, 1, false);
+
+    Decision n61d1 = new Decision("n61d1", "I've got paper and pen with me. Whenever you're ready.", 0, 0);
+    Decision n61d2 = new Decision("n61d2", "Okay. Paper, pen. Let's go.", 0, 0);
+    Decision[] n61d = {n61d1, n61d2};
+    Node n61 = new Node(61, "All I want to do is wish someone farewell.", n61d, 1, false);
+
+    Node n62 = new Node(62, "Thought-to-text won't be the most elegant method, but it's better than what I had before.", emptyDecisions, 1, false);
+
+    Node n63 = new Node(63, "Which was nothing.", emptyDecisions, 1, false);
+
+    Decision n64d1 = new Decision("n64d1", "Take your time.", 0, 0);
+    Decision n64d2 = new Decision("n64d2", "I've got nowhere to be.", 0, 0);
+    Decision[] n64d = {n64d1, n64d2};
+    Node n64 = new Node(64, "It will take me a minute to get this together.", n64d, 1, false);
+
+    Node n65 = new Node(65, "Oh, no.", emptyDecisions, 1, false);
+
+    Node n66 = new Node(66, "Damn it. [Simon}, I have to disconnect. I'll have to...", emptyDecisions, 1, false);
+
+    Decision n67d1 = new Decision("n67d1", "Whoa, whoa. Are you okay?", 0, 0);
+    Decision n67d2 = new Decision("n67d2", "Whoa, whoa. What's going on?", 0, 0);
+    Decision[] n67d = {n67d1, n67d2};
+    Node n67 = new Node(67, "I don't know. Ping me again in a few hours.", n67d, 1, false);
+
+    Node n68 = new Node(68, "no signal", emptyDecisions, 1, false);
+    Node n69 = new Node(69, "status... disconnected", emptyDecisions, 1, false);
+    Node n70 = new Node(70, "shut down", emptyDecisions, 1, false);
+    Node n71 = new Node(71, "3", emptyDecisions, 1, false);
+    Node n72 = new Node(72, "2", emptyDecisions, 1, false);
+    Node n73 = new Node(73, "1", emptyDecisions, 1, false);
+
+    //Wait 3 Hours (If they attempt to play within three hours)
+
+    Decision n74d1 = new Decision("n74d1", "Connected! Hello?? Prosper?", 0, 0);
+    Decision[] n74d = {n74d1};
+    Node n74 = new Node(74, "pinging...\nacquired signal\nstatus...connected", n74d, 0, false);
+
+    Node n75 = new Node(75, "[Simon]?", emptyDecisions, 1, false);
+
+    Decision n76d1 = new Decision("n76d1", "The computer freaked out and shut down. I reconnected as soon as I could. Are you okay? Prosper?", 0, 0);
+    Decision n76d2 = new Decision("n76d2", "The computer freaked out and shut down. I reconnected as soon as I could. What happened? Prosper?", 0, 0);
+    Decision[] n76d = {n76d1, n76d2};
+    Node n76 = new Node(76, "I wasn't sure I'd get the signal back.", n76d, 1, false);
+
+    Node n77 = new Node(77, "I'm fine. Running...a little slow.", emptyDecisions, 1, false);
+
+    Decision n78d1 = new Decision("n78d1", "It wasn't that long ago. A few hours passed.", 0, 0);
+    Decision n78d2 = new Decision("n78d2", "It's been a few hours.", 0, 0);
+    Decision[] n78d = {n78d1, n78d2};
+    Node n78 = new Node(78, "How long has it been since we first spoke?", n78d, 1, false);
+
+    Decision n79d1 = new Decision("n79d1", "Should I be worried?", 0, 0);
+    Decision n79d2 = new Decision("n79d2", "Party too hard?", 1, 0);
+    Decision[] n79d = {n79d1, n79d2};
+    Node n79 = new Node(79, "Okay. Not as bad as I thought.", n79d, 1, false);
+
+    Node n80 = new Node(80, "No, I'm okay.", emptyDecisions, 1, false);
+    Node n81 = new Node(81, "Not exactly.", emptyDecisions, 1, false);
+
+    Decision n82d1 = new Decision("n82d1", "I am.", 0, 0);
+    Decision n82d2 = new Decision("n82d2", "Let's compose some correspondence.", 0, 0);
+    Decision[] n82d = {n82d1, n82d2};
+    Node n82 = new Node(82, "Still willing to help me with that letter, [Simon]?", n82d, 1, false);
+
+    Node n83 = new Node(83, "Thanks.", emptyDecisions, 1, false);
+    Node n84 = new Node(84, "I didn't get the change to put much thought into it, but maybe I don't need to.", emptyDecisions, 1,  false);
+    Node n85 = new Node(85, "Okay. Grace.", emptyDecisions, 1, false);
+
+    Decision n86d1 = new Decision("n86d1", "Go on.", 0, 0);
+    Decision n86d2 = new Decision("n86d2", "Got it.", 0, 0);
+    Decision[] n86d = {n86d1, n86d2};
+    Node n86 = new Node(86, "It's to Grace.", n86d, 1, false);
+
+    Node n87 = new Node(87, "Grace...", emptyDecisions, 1, false);
+    Node n88 = new Node(88, "First off, do not set out to find me.", emptyDecisions, 1, false);
+    Node n89 = new Node(89, "Don't mention Greenland, [Simon]. Or prison.", emptyDecisions, 1, false);
+    Node n90 = new Node(90, "Grace, everything ended in a rush after the Harlem bust. There wasn't anything you or I could do. TUR was prepared for every eventuality.", emptyDecisions, 1, false);
+    Node n91 = new Node(91, "So, they have me. That's the ugly fact. We're right back to the beginning.", emptyDecisions, 1, false);
+    Node n92 = new Node(92, "But that doesn't mean you should try to get my recommissioned. It won't work, even if you could still contact TUR.", emptyDecisions, 1, false);
+    Node n93 = new Node(93, "This is just to...", emptyDecisions, 1, false);
+
+    Decision n94d1 = new Decision("n94d1", "It's okay. This sounds heavy, and it's a weight you probably need off your shoulders.", 0, 0);
+    Decision n94d2 = new Decision("n94d2", "To reach out to this Grace person for the express purpose of telling her not to reach back?", 1, 0);
+    Decision[] n94d = {n94d1, n94d2};
+    Node n94 = new Node(94, "Damn it. I'm not sure this is a good idea, [Simon].", n94d, 1, false);
 
     n0.addChild(n1);
     n1.addChild(n1_5);
@@ -209,6 +390,125 @@ public class Game {
     n29.addChild(n30);
 
     n30.addChild(n20);
+
+    n24.addChild(n31);
+
+    n31.addChild(n32);
+    n31.addChild(n33);
+
+    n32.addChild(n34);
+    n33.addChild(n34);
+
+    n34.addChild(n35);
+
+    n35.addChild(n36);
+
+    n36.addChild(n38);
+    n36.addChild(n37);
+
+    n37.addChild(n38);
+
+    n38.addChild(n39);
+    n38.addChild(n41);
+
+    n39.addChild(n40);
+
+    n41.addChild(n42);
+
+    n42.addChild(n40);
+
+    n42.addChild(n43);
+
+    n43.addChild(n44);
+
+    n44.addChild(n45);
+
+    n45.addChild(n46);
+
+    n46.addChild(n47);
+
+    n47.addChild(n48);
+
+    n48.addChild(n49);
+    n48.addChild(n53);
+
+    n49.addChild(n50);
+
+    n50.addChild(n51);
+
+    n51.addChild(n52);
+
+    n53.addChild(n54);
+
+    n54.addChild(n52);
+
+    n52.addChild(n55);
+
+    n55.addChild(n56);
+    n55.addChild(n57);
+
+    n57.addChild(n56);
+
+    n56.addChild(n58);
+
+    n58.addChild(n59);
+
+    n59.addChild(n60);
+
+    n60.addChild(n61);
+
+    n61.addChild(n62);
+
+    n62.addChild(n63);
+
+    n63.addChild(n64);
+
+    n64.addChild(n65);
+
+    n65.addChild(n66);
+
+    n66.addChild(n67);
+
+    n67.addChild(n68);
+
+    n68.addChild(n69);
+    n69.addChild(n70);
+    n70.addChild(n71);
+    n71.addChild(n72);
+    n72.addChild(n73);
+    n73.addChild(n74);
+
+    n74.addChild(n75);
+
+    n75.addChild(n76);
+
+    n76.addChild(n77);
+
+    n77.addChild(n78);
+
+    n78.addChild(n79);
+
+    n79.addChild(n80);
+    n79.addChild(n81);
+
+    n80.addChild(n82);
+    n81.addChild(n82);
+
+    n82.addChild(n83);
+    n83.addChild(n84);
+    n84.addChild(n85);
+    n85.addChild(n86); //"It's to Grace"
+
+    //Letter to Grace
+    n86.addChild(n87);
+    n87.addChild(n88);
+    n88.addChild(n89);
+    n89.addChild(n90);
+    n90.addChild(n91);
+    n91.addChild(n92);
+    n92.addChild(n93);
+
+    n93.addChild(n94);
 
 
     return n0;
